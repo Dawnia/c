edf28819d2b7debedc10606919b9c4aab058c433
Dawnia <aleksandrov.di@phystech.edu>
2021-02-02 01:00:52 +0300

    first labs

---
+++ lab_1/upr1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hi mir" << endl;
+	cout << "hi" << "mir";
+	
+	return 0;
+}
---
+++ lab_1/upr2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	float a, b;
+	cin >> a >> b;
+	float c = a*a + b*b;
+	c = sqrt(c);
+	cout << c;
+	
+	return 0;
+}
