c0470250eea0821f8b6fbf63f3502908b6555d8c
Dawnia <aleksandrov.di@phystech.edu>
2021-11-20 01:47:22 +0300

    second step++

--- 3 sem/skip_list.cpp
+++ 3 sem/skip_list.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iterator>
 #include <list>
+#include <cassert>
 
 using namespace std;
 
@@ -23,44 +24,55 @@ struct SL_knot{
 };
 
 template <class T>
+class SkipList;
+
+template <class T>
 class SL_iterator: public std::iterator<std::bidirectional_iterator_tag, SL_knot<T>> {
 private:
+	SkipList<T>& thelist;
 	SL_knot<T>* cur;
+	bool ifend;
 
 public:
-	explicit SL_iterator(SL_knot<T>* cur): cur(cur){}
-	SL_iterator(SL_iterator<T>& oth): SL_iterator(oth.cur){}
+	explicit SL_iterator(SL_knot<T>* cur, SkipList<T>& thelist): cur(cur), thelist(thelist){
+		if(cur == nullptr)
+			ifend = true;
+		else
+			ifend = false;
+	}
+	SL_iterator(SL_iterator<T>& oth): SL_iterator(oth.cur, oth.thelist){}
 	SL_iterator(const SL_iterator<T>& oth) = default;
 	SL_iterator& operator = (const SL_iterator<T>& oth) = default;
 	
 	SL_iterator<T>& operator ++ (){
-		cur = cur -> next;
+		if(!ifend)
+			cur = cur -> next;
 		return *this;
 	}
 	
 	SL_iterator<T>& operator--() {
-        cur = cur -> pre;
+		if(!ifend)
+        	cur = cur -> pre;
         return *this;
     }
     
     typename SL_iterator::reference* operator ~ () {
+    	assert(!ifend);
         return cur;
     }
     
-    typename SL_iterator::reference operator * (){
-    	return *cur;
-	}
-    
     T& operator * () {
+    	assert(!ifend);
         return cur -> val;
     }
     
     const T& operator * () const {
+    	assert(!ifend);
         return cur -> val;
     }
     
     bool operator == (const SL_iterator<T>& oth) const {
-        return cur == oth.cur;
+        return ifend == oth.ifend;
     }
     
     bool operator != (const SL_iterator<T>& oth) const {
@@ -70,12 +82,30 @@ public:
 
 
 template <class T>
+struct Floor {
+private:
+	uint64_t level;
+	SL_iterator<T> first;
+	SL_iterator<T> last;
+	bool filled;
+	
+	Floor(SL_iterator<T> first): first(first), last(first){
+		filled = false;
+	}
+	
+	FLoor(){
+		filled = false;
+	}
+};
+
+template <class T>
 class SkipList {
 private:
 	uint64_t len;
 	uint64_t max_len;
 	uint64_t high;
-	list<pair<SL_knot<T>*, bool>> floors;
+	double p;
+	list<Floor<T>> floors;
 	
 	void resize(uint64_t new_len);
 	
@@ -84,15 +114,27 @@ public:
 		len = 0;
 		high = 1;
 		max_len = 8;
+		p = 0.5;
 	}
 	
-	SkipList(SkipList<T>& oth): len(oth.len), high(oth.high), max_len(oth.max_len), floors(oth.floors){}
+	SkipList(SkipList<T>& oth): len(oth.len), high(oth.high), max_len(oth.max_len), floors(oth.floors), p(oth.p){}
 	SkipList(const SkipList<T>& oth) = default;
 	SkipList& operator = (const SkipList<T>& oth) = default;
 	SkipList(SkipList<T>&& oth) = default;
 	SkipList& operator = (SkipList<T>&& oth) = default;
 	
+	SL_iterator<T> end(){
+		return SL_iterator<T>(nullptr);
+	}
 	
+	SL_iterator<T> begin(){
+		if(len)
+			return floors[0] -> first;
+		else
+			return SL_iterator<T>(nullptr, this);
+	}
+	
+	SL_iterator<T> find(T key);
 };
 
 /*
