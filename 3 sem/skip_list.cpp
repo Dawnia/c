2df321d6c08f8cf710ccd6fdd98c41c7b29046fa
Dawnia <aleksandrov.di@phystech.edu>
2021-11-20 19:19:16 +0300

    i still believe though it doesn't work

--- 3 sem/skip_list.cpp
+++ 3 sem/skip_list.cpp
@@ -6,6 +6,13 @@
 using namespace std;
 
 template <class T>
+T vl(list<T>& thelist, uint64_t num){
+	auto it = thelist.begin();
+	for(uint64_t i = 0; i < num && it != thelist.end(); ++i, ++it);
+	return *it;
+}
+
+template <class T>
 struct SL_knot{
 	T val;
 	SL_knot<T>* pre;
@@ -42,8 +49,17 @@ public:
 			ifend = false;
 	}
 	SL_iterator(SL_iterator<T>& oth): SL_iterator(oth.cur, oth.thelist){}
-	SL_iterator(const SL_iterator<T>& oth) = default;
-	SL_iterator& operator = (const SL_iterator<T>& oth) = default;
+	SL_iterator(const SL_iterator<T>& oth): SL_iterator(oth.cur, oth.thelist){}
+	
+	friend class SkipList<T>;
+	
+	SL_iterator& operator = (const SL_iterator<T>& oth){
+		ifend = oth.ifend;
+		cur = oth.cur;
+		thelist = oth.thelist;
+		
+		return *this;
+		}
 	
 	SL_iterator<T>& operator ++ (){
 		if(!ifend)
@@ -60,7 +76,7 @@ public:
         return *this;
     }
     
-    typename SL_iterator<T>::reference* operator ~ () {
+    SL_knot<T>* operator ~ () {
     	assert(!ifend);
         return cur;
     }
@@ -94,19 +110,17 @@ protected:
 
 template <class T>
 struct Floor {
-private:
 	uint64_t level;
 	SL_iterator<T> first;
 	SL_iterator<T> last;
+	const SkipList<T>& thelist;
 	bool filled;
 	
-	Floor(SL_iterator<T> first): first(first), last(first){
+	Floor(uint64_t level, SL_iterator<T>& first, const SkipList<T>& thelist): level(level), first(first), last(first), thelist(thelist){
 		filled = false;
 	}
 	
-	FLoor(){
-		filled = false;
-	}
+	Floor(uint64_t level, const SkipList<T>& thelist): Floor(level, thelist.end(), thelist){}
 };
 
 
@@ -115,6 +129,7 @@ class SkipList {
 private:
 	uint64_t len;
 	uint64_t max_len;
+	uint64_t max_height;
 	uint64_t high;
 	double p;
 	list<Floor<T>> floors;
@@ -127,8 +142,11 @@ public:
 		high = 1;
 		max_len = 8;
 		p = 0.5;
+		
+		max_height = 0;
+		for(uint64_t i = max_len; i > 1; i /= 2)
+			++max_height;
 	}
-	friend class SL_iterator<T>;
 	
 	SkipList(SkipList<T>& oth): len(oth.len), high(oth.high), max_len(oth.max_len), floors(oth.floors), p(oth.p){}
 	SkipList(const SkipList<T>& oth) = default;
@@ -136,33 +154,74 @@ public:
 	SkipList(SkipList<T>&& oth) = default;
 	SkipList& operator = (SkipList<T>&& oth) = default;
 	
+	friend class SL_iterator<T>;
+	
 	SL_iterator<T> end(){
-		return SL_iterator<T>(nullptr);
+		return SL_iterator<T>(nullptr, *this);
 	}
 	
 	SL_iterator<T> begin(){
 		if(len)
 			return floors[0].first;
 		else
-			return SL_iterator<T>(nullptr, this);
+			return SL_iterator<T>(nullptr, *this);
 	}
 	
 	SL_iterator<T>& find(T key){
-		Floor<T>& last_floor = floors.back();
-		SL_iterator<T> i = last_floor.first;
-		for(i; i != this -> end(); ++i){
-			if(*i > key){
-				--i;
-				if(i != floors.end())
-					i.get_lower;
+		SL_iterator<T> i = end();
+		
+		if(len){
+			Floor<T>& last_floor = floors.back();
+			i = last_floor.first;
+			uint64_t curr_level = last_floor.level;
+			for(i; i != end(); ++i){
+				if(*i > key){
+					if(i != end() && i != vl(floors, curr_level).first){
+						--i;
+						i.get_lower();
+					}
+					
+					else if(i != end() && curr_level)
+						i = vl(floors, curr_level - 1).first;
+					
+					else
+						i = end();
+						
+					--curr_level;
+				}
+				
+				else if(*i == key)
+					break;
 			}
-			else if(*i == key)
-				break;
+			
+			for(curr_level; curr_level > 0; --curr_level)
+				i.get_lower();
 		}
+		
 		return i;
 	}
 	
-	
+	void insert(T val){
+		if(!len){
+			SL_knot<T>* first_knot = new SL_knot<T>(val);
+			SL_iterator<T> first_it = SL_iterator<T>(first_knot, *this);
+			Floor<T> first_floor(0, first_it, *this);
+			floors.push_back(first_floor);
+			++len;
+		}
+		
+		else{
+			
+		}
+	}
 };
 
+
+int main(){
+	SkipList<int> mylist;
+	mylist.insert(5);
+	auto it = mylist.find(5);
+	
+	return 0;
+}
 
